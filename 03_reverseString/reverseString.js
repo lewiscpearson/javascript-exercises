45bcde0f2371b0b89043e6bcbe5602b28c2536fc
Lewis Pearson <lewiscpearson@gmail.com>
2024-08-04 16:35:19 +0100

    complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let reversed = "";
+    console.log("test function")
+    for (let i = input.length-1; i>=0; i--){
+        console.log(i + " " + input.length)
+        reversed += input[i];
 
+    }
+    return reversed
 };
-
 // Do not edit below this line
 module.exports = reverseString;
