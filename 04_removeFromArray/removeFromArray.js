3ea96ba91566111814b96f300e553ffe08adf03a
Lewis Pearson <lewiscpearson@gmail.com>
2024-08-04 18:02:57 +0100

    04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,10 @@
-const removeFromArray = function(input, toRemove) {
+const removeFromArray = function(input, ...toRemove) {
     const newArray = [];
-    for (let i = 0; i < toRemove.length; i++){
-        for (let i = 0; i < input.length; i++)  {
-            if (input[i] !== toRemove[i]){
-                newArray.push(input[i]);
-            }
+    input.forEach((item) => {
+        if (!toRemove.includes(item)) {
+          newArray.push(item);
         }
-    }
+      });
     return newArray;
 };
 
