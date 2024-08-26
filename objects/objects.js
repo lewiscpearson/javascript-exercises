a46ea41533383b8571209eb69c57765cf4a0d7ba
Lewis Pearson <lewiscpearson@gmail.com>
2024-08-26 16:56:25 +0100

    multiply numeric object props

--- objects/objects.js
+++ objects/objects.js
@@ -33,3 +33,21 @@ for (let prop in salaries) {
     num = salaries[prop];
     sum1 = sum1 + num;
 }
+
+// before the call
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+
+  
+function multiplyNumeric(obj) {
+    for (let prop in obj) {
+        if (typeof obj[prop] == "number") {
+            obj[prop] = obj[prop]*2
+        }
+    }
+}
+
+multiplyNumeric(menu)
