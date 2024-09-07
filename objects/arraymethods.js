1be077d9b5f395e5294bd45da66e827089b8f15e
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-07 18:16:45 +0100

    second ex, filtering  a range

--- objects/arraymethods.js
+++ objects/arraymethods.js
@@ -1,7 +1,13 @@
+// javascript.info/array-methods Translate border-left-width to borderLeftWidth
+
 function camelize (string) {
     return string
         .split("-")
         .map((word, index) => index == 0 ? word : word[0].toUpperCase()+ word.slice(1))
         .join("");
 }
+
+
+// Filter range
+
 
