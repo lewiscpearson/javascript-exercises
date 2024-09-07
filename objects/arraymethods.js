99e82fc3ba830cccbfd4bfc824870496606bc040
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-07 18:02:19 +0100

    javascript.info array methods ex 1

---
+++ objects/arraymethods.js
@@ -0,0 +1,7 @@
+function camelize (string) {
+    return string
+        .split("-")
+        .map((word, index) => index == 0 ? word : word[0].toUpperCase()+ word.slice(1))
+        .join("");
+}
+
