ff7a2cf10c715c0bfc990ea13f7c3e5cab604dd2
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-08 16:13:43 +0100

    complete exercise 08 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total + currentItem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, currentItem) => {
+    return total * currentItem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = num;
+  if (num === 0 || num === 1)
+    return 1;
+
+  while (num > 1) {
+    num--;
+    result = result * num;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
