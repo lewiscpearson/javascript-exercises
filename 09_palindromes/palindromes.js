58177cac15bb7deab3a23d6b273df486f6c9b7e2
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-08 17:27:33 +0100

    palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^\w\s\']|_/g, "")
+    .replace(/\s+/g, "").toLowerCase();
 
+    return str === Array.from(str).reverse().join('');
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// palindromes('A car, a man, a maraca.')
