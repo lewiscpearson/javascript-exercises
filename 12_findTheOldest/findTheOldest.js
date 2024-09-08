de7f71a6fe97438cca13584f71ba283690efb8b1
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-08 20:58:32 +0100

    ex 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if (!death) {
+        death = new Date().getFullYear() 
+    }
+    return death-birth;
+}
 
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+    let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+    return currentAge > oldestAge ? currentPerson : oldest;
+})
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+ 
