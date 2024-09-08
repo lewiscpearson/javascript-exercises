27df3da27178965a194f0c43392e7299bcd28cfa
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-08 18:11:56 +0100

    fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+if (member<0) {
+    return "OOPS";
+}
 
+    let cur = 0;
+    let prev = 0;
+    let total = 1;
+    for (i=0; i<member; i++) {
+        prev = cur;
+        cur = total;
+        total = cur + prev;
+    }
+    return cur;
 };
 
 // Do not edit below this line
