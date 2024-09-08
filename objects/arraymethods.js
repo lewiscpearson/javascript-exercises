6be22b3aeb848899e99127f223dbf40c0013e710
Lewis Pearson <lewiscpearson@gmail.com>
2024-09-08 12:39:19 +0100

    created keyed object from array

--- objects/arraymethods.js
+++ objects/arraymethods.js
@@ -86,4 +86,21 @@ function shuffle(array) {
     }
   }
 
+// get average age
+function getAverageAge(users) {
+    return users.reduce((prev, user) => prev + user.age, 0) / users.length;
+  }
 
+
+//create keyed object from array
+
+
+let users2 = [
+    {id: 'john', name: "John Smith", age: 20},
+    {id: 'ann', name: "Ann Smith", age: 24},
+    {id: 'pete', name: "Pete Peterson", age: 31},
+  ];
+
+function groupById(users) {
+   return users.reduce((prev, user) => ({...prev, [user.id]: {id: user.id, name: user.name, age: user.age}}), {})
+}
